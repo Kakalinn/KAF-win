c0354bd8258e7282ef369edd83d2fd99974900d6
ELKO Flugstöð - Komuverslun <flekoma@festi.is>
2018-06-22 08:07:45 +0000

    added loop. this automatically updates :)

--- KefAirportFlug/loop.bat
+++ KefAirportFlug/loop.bat
@@ -1,5 +1,12 @@
 @ECHO OFF
+
+start gvim -n -M out.fly -c "map <F5> :e %<CR> | set autoread | set noswapfile"
+
 :loop
-echo %date% %time% - Updating flights
-timeout /t 60 > nul
+echo %date:~5% -%time:~0,-3% - Updating flights (Pressing a key in this window forces a refresh)
+del komur
+wget "https://www.isavia.is/keflavikurflugvollur/flugaaetlun/komur" -q
+echo %date:~5% -%time:~0,-3% (Press F5 to refresh) > out.fly
+stable >> out.fly
+timeout /t 30 > nul
 goto loop
