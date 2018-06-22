81d8bf7e471f04a3c00448427280ea1b9aa49708
ELKO Flugstöð - Komuverslun <flekoma@festi.is>
2018-06-22 11:58:24 +0000

    F5 now returns the cursor to the top of the file for added consistency

--- KefAirportFlug/loop.bat
+++ KefAirportFlug/loop.bat
@@ -8,7 +8,7 @@ wget "https://www.isavia.is/keflavikurflugvollur/flugaaetlun/komur" -q
 echo %date:~5% - %time:~0,-3% (Press F5 to refresh) > out.fly
 kvl >> out.fly
 
-start "" /MAX gvim -n -M out.fly -c "map <F5> :e %<CR> | set autoread | set noswapfile"
+start "" /MAX gvim -n -M out.fly -c "map <F5> :e %<CR>gg | set autoread | set noswapfile"
 
 PING -n 31 127.0.0.1 > nul
 
